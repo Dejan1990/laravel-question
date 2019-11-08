a881e87c46769cb9e5b5d7bdc3d5bf03acb24000
dejan1990 <dejan.simo@hotmail.com>
2019-11-08 10:54:12 +0100

    Question view part one

---
+++ resources/views/questions/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-item-center">
+                        <h2 class="mb-0">All Questions</h2>
+                        <div class="ml-auto">
+                            <a href="#" class="btn btn-outline-secondary">Ask Question</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    @foreach($questions as $question)
+                        <div class="media">
+                            <div class="d-flex flex-column counters">
+                                <div class="vote">
+                                    <strong>{{ $question->votes }}</strong> {{ Str::plural('vote', $question->votes) }}
+                                </div>
+                                <div class="status">
+                                    <strong>{{ $question->answer_count }}</strong> {{ Str::plural('answer', $question->answer_count) }}
+                                </div>
+                                <div class="view">
+                                    {{ $question->views . " " . Str::plural('view', $question->views) }}
+                                </div>
+                            </div>
+                            <div class="media-body">
+                                <div class="d-flex align-items-center">
+                                    <h3 class="mt-0"><a href="{{ $question->url }}">{{ $question->title }}</a></h3>
+                                    <div class="ml-auto">
+                                        <a href="#" class="btn btn-sm btn-outline-info">Edit</a>
+                                        <button class="btn btn-sm btn-outline-danger">Delete</button>
+                                    </div>
+                                </div>
+                                <p class="lead">
+                                    Asked by: <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
+                                    <small class="text-muted">{{ $question->created_date }}</small>
+                                </p>
+                                {{ Str::limit($question->body, 250) }}
+                            </div>
+                        </div>
+                    @endforeach
+
+                    <div class="mx-auto">
+                        {{ $questions->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
